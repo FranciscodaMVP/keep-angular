d1deac9a498d6994293903a9ddd21c92501ceb65
Francisco Valdez <isc.valdezfrancisco@gmail.com>
2017-10-03 13:45:59 -0700

    add functionality with local storage
    add bootstrap

--- notebook.js
+++ notebook.js
@@ -3,23 +3,59 @@ angular
 .controller('notebookController', notebookController)
 .factory('NoteService', NoteService);
 
+// function notebookController($scope, NoteService) {
+// $scope.notes = NoteService.notes;
+// $scope.addNote = function () {
+//     let newNote = {
+//         'text' : $scope.newNote,
+//         'id' : new Date(),
+//         'modified' : '',}
+//     NoteService.notes.push(newNote);
+//     $scope.newNote = {};
+//     }
+    
+// }
+
+
+// function startNotes() {
+//     if (localStorage.getItem('oldNotes')){
+//         NoteService.notes = (JSON.parse(localStorage.getItem('oldNotes')));
+//     }
+// }
+
+function Note (text) {
+    let Note = {
+        'date' : new Date (),
+        'note' : text.text,
+        'modified' : '',
+    };
+    return Note;
+}
+
 function notebookController($scope, NoteService) {
-$scope.notes = NoteService.notes;
-$scope.addNote = function () {
-    let newNote = {
-        'text' : $scope.newNote,
-        'id' : new Date(),
-        'modified' : '',}
-    NoteService.notes.push(newNote);
-    $scope.newNote = {};
-    }
+    
+    $scope.notes = NoteService.notes;
+    $scope.onBlur = function () {
+        let note = Note($scope.newNote);
+        NoteService.notes.push(note);
+        $scope.newNote = {};
+        localStorage.setItem('oldNotes', JSON.stringify($scope.notes));
+        //console.log($scope.notes);
+        }
     
 }
 
 function NoteService() {
-var service = {
-    notes: []
-};
+    if (localStorage.getItem('oldNotes')){
+        var notebook = {
+            notes : (JSON.parse(localStorage.getItem('oldNotes')))
+            
+        }
+        console.log(notebook);
+    } else {var notebook = {
+        notes: [] }
+    }
 
-return service;
+return notebook;
 }
+
